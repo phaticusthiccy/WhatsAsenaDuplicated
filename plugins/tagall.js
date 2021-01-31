d77659e888ec11cf0af0b715bf1087ab5eacf249
Albin Thomas <64305844+AI-VIKI@users.noreply.github.com>
2021-01-31 19:29:25 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -12,7 +12,7 @@ const {MessageType} = require('@adiwajshing/baileys');
 const Language = require('../language');
 const Lang = Language.getString('tagall');
 
-Asena.addCommand({pattern: 'tagall', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
+Asena.addCommand({pattern: 'tagall', fromMe: false, desc: Lang.TAGALL_DESC}, (async (message, match) => {
     grup = await message.client.groupMetadata(message.jid);
     var jids = [];
     mesaj = '';
