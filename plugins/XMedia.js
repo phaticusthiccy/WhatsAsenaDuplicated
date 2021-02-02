258f35ace4f294112bc1a1613236da92046f84bd
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2021-02-02 12:24:53 +0300

    Update XMedia Again

--- plugins/XMedia.js
+++ plugins/XMedia.js
@@ -646,9 +646,9 @@ Asena.addCommand({pattern: 'iosmp3', fromMe: true, dontAddCommandList: true}, (a
 
     ffmpeg(location)
         .outputOptions(["-y", "-vn", "-ar", "44100", "-ac", "2", "-b:a", "192k"])
-        .save('output.ogg')
+        .save('output.aac')
         .on('end', async () => {
-            await message.sendMessage(fs.readFileSync('output.ogg'), MessageType.audio, {mimetype: Mimetype.ogg, ptt: false});
+            await message.sendMessage(fs.readFileSync('output.aac'), MessageType.audio, {mimetype: Mimetype.aac, ptt: false});
         });
     return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 }));
