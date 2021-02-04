fdc62a20333f9e767fa516891eebc7cc665058a3
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2021-02-04 22:47:08 +0300

    Now Only Group Admins Can Use The Tagall ⚠️

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,8 +1,6 @@
 /* Copyright (C) 2020 Yusuf Usta.
-
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
 WhatsAsena - Yusuf Usta
 */
 
@@ -12,7 +10,19 @@ const {MessageType} = require('@adiwajshing/baileys');
 const Language = require('../language');
 const Lang = Language.getString('tagall');
 
+async function checkImAdmin(message, user = message.client.user.jid) {
+    var grup = await message.client.groupMetadata(message.jid);
+    var sonuc = grup['participants'].map((member) => {
+        if (member.id.split('@')[0] === user.split('@')[0] && member.isAdmin) return true; else; return false;
+    });
+    return sonuc.includes(true);
+}
+
 Asena.addCommand({pattern: 'tagall', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
+
+   var im = await checkImAdmin(message);
+   if (!im) return await message.client.sendMessage(message.jid,Lang.ADMİN,MessageType.text);
+
     grup = await message.client.groupMetadata(message.jid);
     var jids = [];
     mesaj = '';
