347a88521636c957db9f06a393ee08523da4464f
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2021-02-06 00:51:53 +0300

    Updated TTP

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -1,3 +1,8 @@
+/* Codded by @phaticusthiccy
+Telegram: t.me/phaticusthiccy
+Instagram: www.instagram.com/kyrie.baran
+*/
+
 const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');
