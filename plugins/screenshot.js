7bdee3ff2dbdd2fb30810e0821657287f97df7bd
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2021-02-06 00:53:12 +0300

    Added Web Screenshot Plugin 🥳

---
+++ plugins/screenshot.js
@@ -0,0 +1,22 @@
+/* Codded by @phaticusthiccy
+Telegram: t.me/phaticusthiccy
+Instagram: www.instagram.com/kyrie.baran
+*/
+
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('webss');
+
+Asena.addCommand({pattern: 'ss ?(.*)', fromMe: true, desc: Lang.SS_DESC}, (async (message, match) => {
+
+    if (match[1] === '') return await message.sendMessage(Lang.LİNK);
+
+    var webimage = await axios.get(`https://nurutomo.herokuapp.com/api/ssweb?delay=1000&url=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by WhatsAsena'})
+
+}));
