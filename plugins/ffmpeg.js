ae18b9d54553bde86545e55dad871b0ef40bdaa7
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2021-02-16 16:55:20 +0300

    Deleted Surpluses on FFMPEG 🛠️

--- plugins/ffmpeg.js
+++ plugins/ffmpeg.js
@@ -14,8 +14,8 @@ const Language = require('../language');
 const Lang = Language.getString('ffmpeg');
 
 
-Asena.addCommand({pattern: 'ffmpeg ?(.*)', fromMe: true, desc: Lang.FF_DESC, warn: 'Dont use on photos! Only for Videos.'}, (async (message, match) => {    
-    if (match[1] === '') return await message.sendMessage('Need Video and Filter Name!\nℹ️ Ex: ```.ffmpeg fade=in:0:30```\nℹ️ Ex: ```.ffmpeg fade=in:0:30, fps=fps=25```');
+Asena.addCommand({pattern: 'ffmpeg ?(.*)', fromMe: true, desc: Lang.FF_DESC}, (async (message, match) => {    
+    if (match[1] === '') return await message.sendMessage('Need Media and Filter Name!\nℹ️ Ex: ```.ffmpeg fade=in:0:30```\nℹ️ Ex: ```.ffmpeg curves=vintage, fps=fps=25```');
     if (message.reply_message.image === false && message.reply_message.video) {
 
         var downloading = await message.client.sendMessage(message.jid,Lang.FF_PROC,MessageType.text);
