7597a1973649fbad8ed7400f892b7c8932f30a23
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2021-02-22 22:52:35 +0100

    Added Instagram and ATTP Plugin / Bug Fix 🥳
    * 🚀 Check for update on wrong command.
    
    * 🌟 Add rainbow effect to the text
    
    * 🌟 Instagram plugin
    
    You can now fetch instagram profiles from WhatsAsena!
    
    * Instagram: send details as caption

---
+++ plugins/instagram.js
@@ -0,0 +1,55 @@
+/*
+# Copyright (C) 2020 MuhammedKpln.
+#
+# WhatsAsena is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# WhatsAsena is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+*/
+
+const Asena = require('../events');
+const { MessageType, } = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const { errorMessage, infoMessage } = require('../helpers');
+const Lang = Language.getString('instagram');
+
+Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: true, usage: Lang.USAGE, desc: Lang.DESC }, (async (message, match) => {
+
+    const userName = match[1]
+
+    if (!userName) return await message.sendMessage(errorMessage(Lang.NEED_WORD));
+
+    await message.sendMessage(infoMessage(Lang.LOADING))
+
+    const { data: { graphql: { user } } } = await axios.get(`https://www.instagram.com/${userName}/?__a=1`)
+
+    const { profile_pic_url_hd, username, biography, edge_followed_by, edge_follow, full_name, is_private } = user
+
+    const profileBuffer = await axios.get(profile_pic_url_hd, { responseType: 'arraybuffer' })
+
+    const msg = `
+    *${Lang.NAME}*: ${full_name}
+    *${Lang.USERNAME}*: ${username}
+    *${Lang.BIO}*: ${biography}
+    *${Lang.FOLLOWERS}*: ${edge_followed_by.count}
+    *${Lang.FOLLOWS}*: ${edge_follow.count}
+    *${Lang.ACCOUNT}*: ${is_private ? Lang.HIDDEN : Lang.PUBLIC}`
+
+    await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: msg })
+
+
+
+
+}));
