28d6946fa8b2d7cc9ec51373a05aaf4b5ae6e62d
Abhi Modzz <78701752+abhimodzz@users.noreply.github.com>
2021-02-25 09:53:24 +0530

    Update admin.js

--- plugins/admin.js
+++ plugins/admin.js
@@ -217,7 +217,7 @@ Asena.addCommand({pattern: 'unmute ?(.*)', fromMe: true, onlyGroup: true, desc: 
     }
 }));
 
-Asena.addCommand({pattern: 'invite ?(.*)', fromMe: true, onlyGroup: true, desc: Lang.INVITE_DESC}, (async (message, match) => {    
+Asena.addCommand({pattern: 'invite ?(.*)', fromMe: false, onlyGroup: true, desc: Lang.INVITE_DESC}, (async (message, match) => {    
     var im = await checkImAdmin(message);
     if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN, MessageType.text);
     var invite = await message.client.groupInviteCode(message.jid);
