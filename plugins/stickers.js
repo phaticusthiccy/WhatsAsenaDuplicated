e8a16017d9908b813b773806771e5e370186c75e
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2021-02-27 01:49:37 +0300

    Added JID for Stickers

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -17,7 +17,8 @@ const Language = require('../language');
 const Lang = Language.getString('sticker');
 
 Asena.addCommand({pattern: 'sticker', fromMe: true, desc: Lang.STICKER_DESC}, (async (message, match) => {    
-    if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);
+
+    if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
     var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
     var location = await message.client.downloadAndSaveMediaMessage({
         key: {
