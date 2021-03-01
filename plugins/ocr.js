512e066f7baddcfcb74651471e2254e9bc89e9c8
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2021-03-01 23:00:35 +0300

    Public Option Added for OCR 🌟

--- plugins/ocr.js
+++ plugins/ocr.js
@@ -15,36 +15,75 @@ const langs = require('langs');
 const Language = require('../language');
 const Lang = Language.getString('ocr');
 
-Asena.addCommand({pattern: 'ocr ?(.*)', fromMe: true, desc: Lang.OCR_DESC}, (async (message, match) => {    
-    if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);    
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({pattern: 'ocr ?(.*)', fromMe: true, desc: Lang.OCR_DESC}, (async (message, match) => {    
+        if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);    
 	var info = await message.reply(Lang.DOWNLOADING);
-    var location = await message.client.downloadAndSaveMediaMessage({
-        key: {
-            remoteJid: message.reply_message.jid,
-            id: message.reply_message.id
-        },
-        message: message.reply_message.data.quotedMessage
-    });
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
 
-    var dil;
-    if (match[1] !== '') {
-        dil = langs.where("1", match[1]);
-    } else {
-        dil = langs.where("1", Config.LANG.toLowerCase());
-    }
+        var dil;
+        if (match[1] !== '') {
+            dil = langs.where("1", match[1]);
+        } else {
+            dil = langs.where("1", Config.LANG.toLowerCase());
+        }
 
-    try {
-        var result = await tesseract.recognize(location, {
-            lang: dil[2]
-        });    
-    } catch (e) {
-        return await message.reply(Lang.ERROR.format(e));
-    }
+        try {
+            var result = await tesseract.recognize(location, {
+                lang: dil[2]
+            });    
+        } catch (e) {
+            return await message.reply(Lang.ERROR.format(e));
+        }
 
-    await info.delete();
-    if ( result === ' ' || result.length == 1 ) {
-        return await message.reply(Lang.ERROR.format(' Empty text'));
-    }
+        await info.delete();
+        if ( result === ' ' || result.length == 1 ) {
+            return await message.reply(Lang.ERROR.format(' Empty text'));
+        }
 
-    return await message.reply(Lang.RESULT.format(dil[2], result));
-}));
+        return await message.reply(Lang.RESULT.format(dil[2], result));
+    }));
+}
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({pattern: 'ocr ?(.*)', fromMe: false, desc: Lang.OCR_DESC}, (async (message, match) => {    
+        if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);    
+	var info = await message.reply(Lang.DOWNLOADING);
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
+
+        var dil;
+        if (match[1] !== '') {
+            dil = langs.where("1", match[1]);
+        } else {
+            dil = langs.where("1", Config.LANG.toLowerCase());
+        }
+
+        try {
+            var result = await tesseract.recognize(location, {
+                lang: dil[2]
+            });    
+        } catch (e) {
+            return await message.reply(Lang.ERROR.format(e));
+        }
+
+        await info.delete();
+        if ( result === ' ' || result.length == 1 ) {
+            return await message.reply(Lang.ERROR.format(' Empty text'));
+        }
+
+        return await message.reply(Lang.RESULT.format(dil[2], result));
+    }));
+}
