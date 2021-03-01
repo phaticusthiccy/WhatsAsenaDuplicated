03346a8d2c60d4e68499b5270d4a373f965fc42b
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2021-03-01 21:55:36 +0300

    Public Option Added for Dyno Usage 🌟

--- plugins/heroku.js
+++ plugins/heroku.js
@@ -46,36 +46,73 @@ Asena.addCommand({pattern: 'shutdown', fromMe: true, desc: Lang.SHUTDOWN_DESC}, 
     });
 }));
 
-Asena.addCommand({pattern: 'dyno', fromMe: true, desc: Lang.DYNO_DESC}, (async (message, match) => {
 
-    heroku.get('/account').then(async (account) => {
-        // have encountered some issues while calling this API via heroku-client
-        // so let's do it manually
-        url = "https://api.heroku.com/accounts/" + account.id + "/actions/get-quota"
-        headers = {
-            "User-Agent": "Chrome/80.0.3987.149 Mobile Safari/537.36",
-            "Authorization": "Bearer " + Config.HEROKU.API_KEY,
-            "Accept": "application/vnd.heroku+json; version=3.account-quotas",
-        }
-        await got(url, {headers: headers}).then(async (res) => {
-           const resp = JSON.parse(res.body);
-           total_quota = Math.floor(resp.account_quota);
-           quota_used = Math.floor(resp.quota_used);         
-           percentage = Math.round((quota_used / total_quota) * 100);
-           remaining = total_quota - quota_used;
-           await message.client.sendMessage(
-                message.jid,
-                Lang.DYNO_TOTAL + ": ```{}```\n\n".format(secondsToHms(total_quota))  + 
-                Lang.DYNO_USED + ": ```{}```\n".format(secondsToHms(quota_used)) +  
-                Lang.PERCENTAGE + ": ```{}```\n\n".format(percentage) +
-                Lang.DYNO_LEFT + ": ```{}```\n".format(secondsToHms(remaining)),
-                MessageType.text
-           );
-        }).catch(async (err) => {
-            await message.client.sendMessage(message.jid,err.message, MessageType.text);     
-        });        
-    });
-}));
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({pattern: 'dyno', fromMe: true, desc: Lang.DYNO_DESC}, (async (message, match) => {
+
+        heroku.get('/account').then(async (account) => {
+            // have encountered some issues while calling this API via heroku-client
+            // so let's do it manually
+            url = "https://api.heroku.com/accounts/" + account.id + "/actions/get-quota"
+            headers = {
+                "User-Agent": "Chrome/80.0.3987.149 Mobile Safari/537.36",
+                "Authorization": "Bearer " + Config.HEROKU.API_KEY,
+                "Accept": "application/vnd.heroku+json; version=3.account-quotas",
+            }
+            await got(url, {headers: headers}).then(async (res) => {
+               const resp = JSON.parse(res.body);
+               total_quota = Math.floor(resp.account_quota);
+               quota_used = Math.floor(resp.quota_used);         
+               percentage = Math.round((quota_used / total_quota) * 100);
+               remaining = total_quota - quota_used;
+               await message.client.sendMessage(
+                    message.jid,
+                    Lang.DYNO_TOTAL + ": ```{}```\n\n".format(secondsToHms(total_quota))  + 
+                    Lang.DYNO_USED + ": ```{}```\n".format(secondsToHms(quota_used)) +  
+                    Lang.PERCENTAGE + ": ```{}```\n\n".format(percentage) +
+                    Lang.DYNO_LEFT + ": ```{}```\n".format(secondsToHms(remaining)),
+                    MessageType.text
+               );
+            }).catch(async (err) => {
+                await message.client.sendMessage(message.jid,err.message, MessageType.text);     
+            });        
+        });
+    }));
+}
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({pattern: 'dyno', fromMe: false, desc: Lang.DYNO_DESC}, (async (message, match) => {
+
+        heroku.get('/account').then(async (account) => {
+            // have encountered some issues while calling this API via heroku-client
+            // so let's do it manually
+            url = "https://api.heroku.com/accounts/" + account.id + "/actions/get-quota"
+            headers = {
+                "User-Agent": "Chrome/80.0.3987.149 Mobile Safari/537.36",
+                "Authorization": "Bearer " + Config.HEROKU.API_KEY,
+                "Accept": "application/vnd.heroku+json; version=3.account-quotas",
+            }
+            await got(url, {headers: headers}).then(async (res) => {
+               const resp = JSON.parse(res.body);
+               total_quota = Math.floor(resp.account_quota);
+               quota_used = Math.floor(resp.quota_used);         
+               percentage = Math.round((quota_used / total_quota) * 100);
+               remaining = total_quota - quota_used;
+               await message.client.sendMessage(
+                    message.jid,
+                    Lang.DYNO_TOTAL + ": ```{}```\n\n".format(secondsToHms(total_quota))  + 
+                    Lang.DYNO_USED + ": ```{}```\n".format(secondsToHms(quota_used)) +  
+                    Lang.PERCENTAGE + ": ```{}```\n\n".format(percentage) +
+                    Lang.DYNO_LEFT + ": ```{}```\n".format(secondsToHms(remaining)),
+                    MessageType.text
+               );
+            }).catch(async (err) => {
+                await message.client.sendMessage(message.jid,err.message, MessageType.text);     
+            });        
+        });
+    }));
+}
 
 Asena.addCommand({pattern: 'setvar ?(.*)', fromMe: true, desc: Lang.SETVAR_DESC}, (async(message, match) => {
 
