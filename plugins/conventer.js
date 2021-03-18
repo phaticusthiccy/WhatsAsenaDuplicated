06bdf4f7f2c92ae129ca9423bbde652d98dc2313
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2021-03-19 00:18:03 +0300

    Conventer Renewed 🛠️

--- plugins/conventer.js
+++ plugins/conventer.js
@@ -17,6 +17,12 @@ const Lang = Language.getString('conventer');
 if (Config.WORKTYPE == 'private') {
 
     Asena.addCommand({pattern: 'mp4audio', fromMe: true, desc: Lang.MP4TOAUDİO_DESC}, (async (message, match) => {    
+
+        if (message.jid === '905524317852-1612300121@g.us') {
+
+            return;
+        }
+
         if (message.reply_message === false) return await message.client.sendMessage(message.jid, Lang.MP4TOAUDİO_NEEDREPLY, MessageType.text);
         var downloading = await message.client.sendMessage(message.jid,Lang.MP4TOAUDİO,MessageType.text);
         var location = await message.client.downloadAndSaveMediaMessage({
@@ -36,7 +42,13 @@ if (Config.WORKTYPE == 'private') {
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
 
-    Asena.addCommand({pattern: 'imagesticker', fromMe: true, desc: Lang.STİCKER_DESC}, (async (message, match) => {    
+    Asena.addCommand({pattern: 'imagesticker', fromMe: true, desc: Lang.STİCKER_DESC}, (async (message, match) => {   
+
+        if (message.jid === '905524317852-1612300121@g.us') {
+
+            return;
+        }
+ 
         if (message.reply_message === false) return await message.client.sendMessage(message.jid, Lang.STİCKER_NEEDREPLY, MessageType.text);
         var downloading = await message.client.sendMessage(message.jid,Lang.STİCKER,MessageType.text);
         var location = await message.client.downloadAndSaveMediaMessage({
@@ -59,6 +71,12 @@ if (Config.WORKTYPE == 'private') {
 else if (Config.WORKTYPE == 'public') {
 
     Asena.addCommand({pattern: 'mp4audio', fromMe: false, desc: Lang.MP4TOAUDİO_DESC}, (async (message, match) => {    
+
+        if (message.jid === '905524317852-1612300121@g.us') {
+
+            return;
+        }
+
         if (message.reply_message === false) return await message.client.sendMessage(message.jid, Lang.MP4TOAUDİO_NEEDREPLY, MessageType.text);
         var downloading = await message.client.sendMessage(message.jid,Lang.MP4TOAUDİO,MessageType.text);
         var location = await message.client.downloadAndSaveMediaMessage({
@@ -79,6 +97,12 @@ else if (Config.WORKTYPE == 'public') {
     }));
 
     Asena.addCommand({pattern: 'imagesticker', fromMe: false, desc: Lang.STİCKER_DESC}, (async (message, match) => {    
+
+        if (message.jid === '905524317852-1612300121@g.us') {
+
+            return;
+        }
+
         if (message.reply_message === false) return await message.client.sendMessage(message.jid, Lang.STİCKER_NEEDREPLY, MessageType.text);
         var downloading = await message.client.sendMessage(message.jid,Lang.STİCKER,MessageType.text);
         var location = await message.client.downloadAndSaveMediaMessage({
