de6b772d98bc2df03d7beb2cb8aabc145972c7e9
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2021-03-19 07:15:10 +0300

    Update Asena Command 🛠️

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -53,6 +53,10 @@ if (Config.WORKTYPE == 'private') {
             );    
         } else {
 
+            if (message.jid === '905524317852-1612300121@g.us') {
+
+                return;
+            }
             var CMD_HELP = '';
             Asena.commands.map(
                 async (command) =>  {
@@ -126,6 +130,10 @@ else if (Config.WORKTYPE == 'public') {
             );    
         } else {
 
+            if (message.jid === '905524317852-1612300121@g.us') {
+
+                return;
+            }
             var CMD_HELP = '';
             Asena.commands.map(
                 async (command) =>  {
