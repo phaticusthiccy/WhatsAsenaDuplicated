8bd7edf2d16d18c303f3ae6258a6cb0a2feb9f8c
4anto <82128294+4anto@users.noreply.github.com>
2021-04-11 09:12:14 +0800

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -67,7 +67,7 @@ if (Config.WORKTYPE == 'private') {
                             HANDLER = '.';
                         }
                         CMD_HELP += '*🌀✥ ' + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmatch[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
-                        if (command.desc !== '') CMD_HELP += '*✉✥ ' + Lang.DESC + ':* ' + command.desc + (command.warn === '' ? \n\n' : '\n');
+                        if (command.desc !== '') CMD_HELP += '*✉✥ ' + Lang.DESC + ':* ' + command.desc + (command.warn === '' ? '\n\n' : '\n');
                         if (command.usage !== '') CMD_HELP += '*⌨️ ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
                         if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n';
 
