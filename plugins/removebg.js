d5bfcc3acd26dbbe595ad31abbfa1b8662df6379
4anto <82128294+4anto@users.noreply.github.com>
2021-04-12 17:46:58 +0800

    Update removebg.js

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -52,7 +52,7 @@ if (Config.WORKTYPE == 'private') {
 		    fs.createWriteStream('rbg.png')
         );
     
-        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'WhatsAsena.png', mimetype: Mimetype.png});
+        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'WhatsSohuman.png', mimetype: Mimetype.png});
         await load.delete();
     }));
 }
@@ -88,7 +88,7 @@ else if (Config.WORKTYPE == 'public') {
 		    fs.createWriteStream('rbg.png')
         );
     
-        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'WhatsAsena.png', mimetype: Mimetype.png});
+        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'WhatsSohuman.png', mimetype: Mimetype.png});
         await load.delete();
     }));
 }
