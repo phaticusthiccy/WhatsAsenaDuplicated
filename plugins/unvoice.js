f63fc8c88f43fe02ad9f0b41342dcf274081fb00
Thiccy <birhic023@gmail.com>
2021-04-22 22:08:53 +0300

    Improved Unvoice 🥳

--- plugins/unvoice.js
+++ plugins/unvoice.js
@@ -60,5 +60,25 @@ else if (Config.WORKTYPE == 'public') {
             });
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
+    Asena.addCommand({pattern: 'unvoice', fromMe: true, desc: Lang.UV_DESC}, (async (message, match) => {    
+
+        if (message.reply_message === false) return await message.sendMessage(Lang.UV_REPLY);
+        var downloading = await message.client.sendMessage(message.jid,Lang.UV_PROC,MessageType.text);
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
+
+        ffmpeg(location)
+            .format('mp3')
+            .save('output.mp3')
+            .on('end', async () => {
+                await message.sendMessage(fs.readFileSync('output.mp3'), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true});
+            });
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    }));
 }
 
