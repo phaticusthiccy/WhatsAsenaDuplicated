3320faf729b90d232b9360da43b5db5bf311e044
Thiccy <birhic023@gmail.com>
2021-04-22 20:50:15 +0300

    Improved System Stats and Added Variables for Alıve Message 🥳
    Now, you can set varibale in alive message. 
    {pp} = It was enough to use it 1 time. It gets your profile pic.
    {version} = It gets bot's version
    {info} = It gets your status

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -12,6 +12,7 @@ const {MessageType} = require('@adiwajshing/baileys');
 const {spawnSync} = require('child_process');
 const Config = require('../config');
 const chalk = require('chalk');
+const Axios = require('axios');
 
 const Language = require('../language');
 const Lang = Language.getString('system_stats');
@@ -25,7 +26,17 @@ if (Config.WORKTYPE == 'private') {
             await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/asenaremaster' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.ALIVEMSG + '\n*Powered by WhatsAsena*', MessageType.text);
+            const payload = Config.ALIVEMSG
+            const status = await message.client.getStatus()
+            const ppUrl = await message.client.getProfilePicture() 
+            const resim = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
+
+            if (!payload.includes('{pp}')) {
+                await message.client.sendMessage(message.jid,payload.replace('{version}', Config.VERSION).replace('{info}', `${status.status}`), MessageType.text);
+            }
+            else if (payload.includes('{pp}')) {
+                await message.sendMessage(Buffer.from(resim.data), MessageType.image, { caption: payload.replace('{version}', Config.VERSION).replace('{pp}', '').replace('{info}', `${status.status}`) });
+            }
         }
     }));
 
@@ -45,11 +56,49 @@ else if (Config.WORKTYPE == 'public') {
             await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/asenaremaster' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.ALIVEMSG + '\n*Powered by WhatsAsena*', MessageType.text);
+            const payload = Config.ALIVEMSG
+            const status = await message.client.getStatus()
+            const ppUrl = await message.client.getProfilePicture() 
+            const resim = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
+
+            if (!payload.includes('{pp}')) {
+                await message.client.sendMessage(message.jid,payload.replace('{version}', Config.VERSION).replace('{info}', `${status.status}`), MessageType.text);
+            }
+            else if (payload.includes('{pp}')) {
+                await message.sendMessage(Buffer.from(resim.data), MessageType.image, { caption: payload.replace('{version}', Config.VERSION).replace('{pp}', '').replace('{info}', `${status.status}`) });
+            }
         }
     }));
 
     Asena.addCommand({pattern: 'sysd', fromMe: false, desc: Lang.SYSD_DESC}, (async (message, match) => {
+
+        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+        await message.sendMessage(
+            '```' + child + '```', MessageType.text
+        );
+    }));
+
+    Asena.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+
+        if (Config.ALIVEMSG == 'default') {
+            await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/asenaremaster' , MessageType.text);
+        }
+        else {
+            const payload = Config.ALIVEMSG
+            const status = await message.client.getStatus()
+            const ppUrl = await message.client.getProfilePicture() 
+            const resim = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
+
+            if (!payload.includes('{pp}')) {
+                await message.client.sendMessage(message.jid,payload.replace('{version}', Config.VERSION).replace('{info}', `${status.status}`), MessageType.text);
+            }
+            else if (payload.includes('{pp}')) {
+                await message.sendMessage(Buffer.from(resim.data), MessageType.image, { caption: payload.replace('{version}', Config.VERSION).replace('{pp}', '').replace('{info}', `${status.status}`) });
+            }
+        }
+    }));
+
+    Asena.addCommand({pattern: 'sysd', fromMe: true, desc: Lang.SYSD_DESC}, (async (message, match) => {
 
         const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
         await message.sendMessage(
