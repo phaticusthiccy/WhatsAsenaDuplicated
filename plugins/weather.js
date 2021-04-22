233466af9b1ab81dd8a58d6e2a46056ed5fd6cd4
Thiccy <birhic023@gmail.com>
2021-04-22 22:13:16 +0300

    Improved Weather 🥳

--- plugins/weather.js
+++ plugins/weather.js
@@ -54,4 +54,21 @@ if (Config.WORKTYPE == 'public') {
 		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
 	    }
     });
+    Asena.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: true}, async (message, match) => {
+
+	    if (match[1] === '') return await message.reply(Lang.NEED_LOCATION);
+	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=tr`;
+	    try {
+		    const response = await got(url);
+		    const json = JSON.parse(response.body);
+		    if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📍 ' + Lang.LOCATION +':* ```' + match[1] + '```\n\n' +
+		    '*☀ ' + Lang.TEMP +':* ```' + json.main.temp_max + '°```\n' + 
+		    '*ℹ ' + Lang.DESC +':* ```' + json.weather[0].description + '```\n' +
+		    '*☀ ' + Lang.HUMI +':* ```%' + json.main.humidity + '```\n' + 
+		    '*💨 ' + Lang.WIND +':* ```' + json.wind.speed + 'm/s```\n' + 
+		    '*☁ ' + Lang.CLOUD +':* ```%' + json.clouds.all + '```\n', MessageType.text);
+	    } catch {
+		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
+	    }
+    });
 }
