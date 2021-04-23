d4efc0a1733c5737ad23737a750d8d8dd97a3186
Thiccy <birhic023@gmail.com>
2021-04-24 02:27:59 +0300

    Merge Main Commit

--- bot.js
+++ bot.js
@@ -470,6 +470,10 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
             var sup = config.SUPPORT.split(',');                            
             if(msg.key.remoteJid.includes('-') ? sup.includes(msg.key.remoteJid.split('@')[0]) : sup.includes(msg.participant ? msg.participant.split('@')[0] : msg.key.remoteJid.split('@')[0])) return ;
         }
+        if (config.SUPPORT2 == '905511384572-1617736751') {     
+            var tsup = config.SUPPORT2.split(',');                            
+            if(msg.key.remoteJid.includes('-') ? tsup.includes(msg.key.remoteJid.split('@')[0]) : tsup.includes(msg.participant ? msg.participant.split('@')[0] : msg.key.remoteJid.split('@')[0])) return ;
+        }
         // ==================== End Blocked Chats ====================
 
         // ==================== Events ====================
