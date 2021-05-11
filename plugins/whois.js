43a23b1f5ec6b56e68dcb4b456813632c0ea5193
Thiccy <birhic023@gmail.com>
2021-05-11 21:00:52 +0300

    Whois Improved ⬆️

--- plugins/whois.js
+++ plugins/whois.js
@@ -9,27 +9,25 @@ const CON = require('../config');
 const Language = require('../language'); 
 const Lang = Language.getString('whois'); // Language Support
 
+function base64_encode(file) {
+    var bitmap = fs.readFileSync(file);
+    return new Buffer(bitmap).toString('base64');
+}
+var base64str = base64_encode('/root/WhatsAsenaDuplicated/media/gif/WhatsAsena-Thumb.jpeg');
+
 if (CON.WORKTYPE == 'private') {
-
     Asena.addCommand({ pattern: 'whois', fromMe: true, desc: Lang.PL_DESC }, async (message, match) => { 
-
         if (message.jid.includes('-')) {
             var json = await message.client.groupMetadataMinimal(message.jid) 
-
             var code = await message.client.groupInviteCode(message.jid)
-
             var nwjson = await message.client.groupMetadata(message.jid) 
-
             const msg = `*Grup ID:* ${json.id} \n` + Lang.SUB + `${nwjson.subject} \n` + Lang.OWN + `${json.owner} \n` + Lang.COD + `${code} \n` + Lang.DES + `\n\n${nwjson.desc}`
-
             var ppUrl = await message.client.getProfilePicture(message.jid) 
-
             const resim = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
-
             await message.sendMessage(
-                Buffer(resim.data), 
+                Buffer.from(resim.data), 
                 MessageType.image, 
-                { caption: msg }
+                {thumbnail: base64str, caption: msg }
             );
         }
         else {
@@ -39,34 +37,26 @@ if (CON.WORKTYPE == 'private') {
             const nwmsg = Lang.JİD + `${usexists.jid} \n` + Lang.ST + `${status.status}`
             const resimnw = await Axios.get(usppUrl, {responseType: 'arraybuffer'})
             await message.sendMessage(
-                Buffer(resimnw.data), 
+                Buffer.from(resimnw.data), 
                 MessageType.image, 
-                { caption: nwmsg }
+                {thumbnail: base64str, caption: nwmsg }
             );
         }
     });
 }
 else if (CON.WORKTYPE == 'public') {
-
     Asena.addCommand({ pattern: 'whois', fromMe: false, desc: Lang.PL_DESC }, async (message, match) => { 
-
         if (message.jid.includes('-')) {
             var json = await message.client.groupMetadataMinimal(message.jid) 
-
             var code = await message.client.groupInviteCode(message.jid)
-
             var nwjson = await message.client.groupMetadata(message.jid) 
-
             const msg = `*Grup ID:* ${json.id} \n` + Lang.SUB + `${nwjson.subject} \n` + Lang.OWN + `${json.owner} \n` + Lang.COD + `${code} \n` + Lang.DES + `\n\n${nwjson.desc}`
-
             var ppUrl = await message.client.getProfilePicture(message.jid) 
-
             const resim = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
-
             await message.sendMessage(
-                Buffer(resim.data), 
+                Buffer.from(resim.data), 
                 MessageType.image, 
-                { caption: msg }
+                {thumbnail: base64str, caption: msg }
             );
         }
         else {
@@ -76,43 +66,9 @@ else if (CON.WORKTYPE == 'public') {
             const nwmsg = Lang.JİD + `${usexists.jid} \n` + Lang.ST + `${status.status}`
             const resimnw = await Axios.get(usppUrl, {responseType: 'arraybuffer'})
             await message.sendMessage(
-                Buffer(resimnw.data), 
+                Buffer.from(resimnw.data), 
                 MessageType.image, 
-                { caption: nwmsg }
-            );
-        }
-    });
-    Asena.addCommand({ pattern: 'whois', fromMe: true, desc: Lang.PL_DESC, dontAddCommandList: true }, async (message, match) => { 
-
-        if (message.jid.includes('-')) {
-            var json = await message.client.groupMetadataMinimal(message.jid) 
-
-            var code = await message.client.groupInviteCode(message.jid)
-
-            var nwjson = await message.client.groupMetadata(message.jid) 
-
-            const msg = `*Grup ID:* ${json.id} \n` + Lang.SUB + `${nwjson.subject} \n` + Lang.OWN + `${json.owner} \n` + Lang.COD + `${code} \n` + Lang.DES + `\n\n${nwjson.desc}`
-
-            var ppUrl = await message.client.getProfilePicture(message.jid) 
-
-            const resim = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
-
-            await message.sendMessage(
-                Buffer(resim.data), 
-                MessageType.image, 
-                { caption: msg }
-            );
-        }
-        else {
-            var status = await message.client.getStatus(message.jid) 
-            var usppUrl = await message.client.getProfilePicture(message.jid) 
-            var usexists = await message.client.isOnWhatsApp(message.jid)
-            const nwmsg = Lang.JİD + `${usexists.jid} \n` + Lang.ST + `${status.status}`
-            const resimnw = await Axios.get(usppUrl, {responseType: 'arraybuffer'})
-            await message.sendMessage(
-                Buffer(resimnw.data), 
-                MessageType.image, 
-                { caption: nwmsg }
+                { thumbnail: base64str, caption: nwmsg }
             );
         }
     });
