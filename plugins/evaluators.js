06aa1951a5a2a2d66b0b344c892b3db60a160e36
Thiccy <birhic023@gmail.com>
2021-06-14 13:15:22 +0300

    RegExp Fix - Bug Fix ⚒️

--- plugins/evaluators.js
+++ plugins/evaluators.js
@@ -15,20 +15,22 @@ const Language = require('../language');
 const Lang = Language.getString('evaluators');
 const SLang = Language.getString('conventer');
 
-Asena.addCommand({pattern: 'term?(.*)', fromMe: true, desc: Lang.TERM_DESC}, (async (message, match) => {    
+Asena.addCommand({pattern: 'term ?(.*)', fromMe: true, desc: Lang.TERM_DESC}, (async (message, match) => {    
     var user = message.client.user.name
-    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.GIVE_ME_CODE,MessageType.text);
+    var id = message.jid
+    if (match[1] === '') return await message.client.sendMessage(id,Lang.GIVE_ME_CODE,MessageType.text);
 
     exec(match[1], async (err, stdout, stderr) => {
         if (err) {
-            return await message.client.sendMessage(message.jid,'```' + user + ':~# ' + match[1] + '\n' + err + '```',MessageType.text);
+            return await message.client.sendMessage(id,'```' + user + ':~# ' + match[1] + '\n' + err + '```',MessageType.text);
         }
         
-        return await message.client.sendMessage(message.jid,'```' + user + ':~# ' + match[1] + '\n' + stdout + '```',MessageType.text);
+        return await message.client.sendMessage(id,'```' + user + ':~# ' + match[1] + '\n' + stdout + '```',MessageType.text);
       });
 }));
 let wk = Config.WORKTYPE == 'public' ? false : true
 Asena.addCommand({pattern: 'mediainfo$', fromMe: wk, desc: Lang.MDESC}, (async (message, match) => {    
+    var id = message.jid
     if (message.reply_message.video) {
         var location = await message.client.downloadAndSaveMediaMessage({
             key: {
@@ -40,7 +42,7 @@ Asena.addCommand({pattern: 'mediainfo$', fromMe: wk, desc: Lang.MDESC}, (async (
         exec('mv ' + location + ' /root/WhatsAsenaDuplicated/vid.mp4')
         exec('ffprobe -hide_banner -loglevel fatal -show_error -show_format -show_streams -show_programs -show_chapters -show_private_data -print_format json /root/WhatsAsenaDuplicated/vid.mp4', async (err, st, stderr) => {
             if (err) {
-                return await message.client.sendMessage(message.jid,'*Error:* \n\n' + err,MessageType.text);
+                return await message.client.sendMessage(id,'*Error:* \n\n' + err,MessageType.text);
             }
             var stdout = JSON.parse(st)
             let
@@ -62,8 +64,8 @@ Asena.addCommand({pattern: 'mediainfo$', fromMe: wk, desc: Lang.MDESC}, (async (
                 schannel = Config.LANG == 'TR' || Config.LANG == 'AZ' ? '\n*Ses Kanalları:* ' : '\n*Audio Channels:* '
                 schome = Config.LANG == 'TR' || Config.LANG == 'AZ' ? '\n*Ses Kanalı Yerleşimi:* ' : '\n*Audio Channel Layout:* '
             var msgi = vsize + stdout.format.size / 1000000 + 'MB' + vlength + stdout.streams[0].duration + second + vrvalue + stdout.streams[0].width + 'p' + vpvalue + stdout.streams[0].width + 'x' + stdout.streams[0].height + vpformat + stdout.streams[0].pix_fmt + vcprofile + stdout.streams[0].codec_name + vctag + stdout.streams[0].codec_tag_string + srvalue + stdout.streams[0].sample_aspect_ratio + vrvalue + stdout.streams[0].display_aspect_ratio + vfps + stdout.streams[0].r_frame_rate.split('/')[0] + vavgfps + stdout.streams[0].avg_frame_rate.split('/')[0] + sctip + stdout.streams[1].codec_name + sctag + stdout.streams[1].codec_tag_string + shzvalue + stdout.streams[1].sample_rate + schannel + stdout.streams[1].channels + schome + stdout.streams[1].channel_layout            
-            return await message.client.sendMessage(message.jid,msgi,MessageType.text);
+            return await message.client.sendMessage(id,msgi,MessageType.text);
         });
-    } else { return await message.client.sendMessage(message.jid,SLang.MP4TOAUDİO_NEEDREPLY, MessageType.text)
+    } else { return await message.client.sendMessage(id,SLang.MP4TOAUDİO_NEEDREPLY, MessageType.text)
     }
 }));
