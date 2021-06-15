3b2a0dd4275d7a67659ef9a18b24930264227447
Thiccy <birhic023@gmail.com>
2021-06-15 23:50:47 +0300

    Improved Meme ⬆️

--- plugins/memes.js
+++ plugins/memes.js
@@ -91,39 +91,4 @@ else if (Config.WORKTYPE == 'public') {
             await info.delete();    
         });
     }));
-    Asena.addCommand({pattern: 'meme ?(.*)', fromMe: true, desc: Lang.MEMES_DESC, dontAddCommandList: true}, (async (message, match) => {   
-
-        if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
-        var topText, bottomText;
-        if (match[1].includes(';')) {
-            var split = match[1].split(';');
-            topText = split[1];
-            bottomText = split[0];
-        }
-	    else {
-            topText = match[1];
-            bottomText = '';
-        }
-    
-	    var info = await message.reply(Lang.DOWNLOADING);
-	
-        var location = await message.client.downloadAndSaveMediaMessage({
-            key: {
-                remoteJid: message.reply_message.jid,
-                id: message.reply_message.id
-            },
-            message: message.reply_message.data.quotedMessage
-        }); 
-    
-	    memeMaker({
-            image: location,         
-            outfile: 'asena-meme.png',
-            topText: topText,
-            bottomText: bottomText,
-        }, async function(err) {
-            if(err) throw new Error(err)
-            await message.client.sendMessage(message.jid, fs.readFileSync('asena-meme.png'), MessageType.image, {filename: 'asena-meme.png', mimetype: Mimetype.png, caption: 'Made by WhatsAsena'});
-            await info.delete();    
-        });
-    }));
 }
