6dbf249282d871a87a0b879205d16bb0c021bb81
Thiccy <birhic023@gmail.com>
2021-06-16 00:55:56 +0300

    Improved TTP ⬆️

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -14,19 +14,19 @@ const Lang = Language.getString('ttp');
 
 
 if (Config.WORKTYPE == 'private') {
-    Asena.addCommand({ pattern: 'ttp?(.*)', fromMe: true, desc: Lang.TTP_DESC }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'ttp ?(.*)', fromMe: true, desc: Lang.TTP_DESC }, (async (message, match) => {
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
         var uri = encodeURI(match[1])
         var ttinullimage = await axios.get('https://api.xteam.xyz/ttp?file&text=' + uri, { responseType: 'arraybuffer' })
         await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by WhatsAsena' })
     }));
-    Asena.addCommand({ pattern: 'attp?(.*)', fromMe: true, desc: Lang.ATTP_DESC }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'attp ?(.*)', fromMe: true, desc: Lang.ATTP_DESC }, (async (message, match) => {
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
         var uri = encodeURI(match[1])
         var ttinullimage = await axios.get('https://api.xteam.xyz/attp?file&text=' + uri, { responseType: 'arraybuffer' })
         await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
     }));
-    Asena.addCommand({ pattern: 'glowttp?(.*)', fromMe: true, desc: Lang.GLOW_DESC }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'glowttp ?(.*)', fromMe: true, desc: Lang.GLOW_DESC }, (async (message, match) => {
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
         var uri = encodeURI(match[1])
         var ttinullimage = await axios.get('https://videfikri.com/api/textmaker/glowingneon/?text=' + uri, { responseType: 'arraybuffer' })
@@ -34,19 +34,19 @@ if (Config.WORKTYPE == 'private') {
     }));
 }
 else if (Config.WORKTYPE == 'public') {
-    Asena.addCommand({ pattern: 'ttp?(.*)', fromMe: false, desc: Lang.TTP_DESC }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'ttp ?(.*)', fromMe: false, desc: Lang.TTP_DESC }, (async (message, match) => {
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
         var uri = encodeURI(match[1])
         var ttinullimage = await axios.get('https://api.xteam.xyz/ttp?file&text=' + uri, { responseType: 'arraybuffer' })
         await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by WhatsAsena' })
     }));
-    Asena.addCommand({ pattern: 'attp?(.*)', fromMe: false, desc: Lang.ATTP_DESC }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'attp ?(.*)', fromMe: false, desc: Lang.ATTP_DESC }, (async (message, match) => {
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
         var uri = encodeURI(match[1])
         var ttinullimage = await axios.get('https://api.xteam.xyz/attp?file&text=' + uri, { responseType: 'arraybuffer' })
         await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
     }));
-    Asena.addCommand({ pattern: 'glowttp?(.*)', fromMe: false, desc: Lang.GLOW_DESC }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'glowttp ?(.*)', fromMe: false, desc: Lang.GLOW_DESC }, (async (message, match) => {
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
         var uri = encodeURI(match[1])
         var ttinullimage = await axios.get('https://videfikri.com/api/textmaker/glowingneon/?text=' + uri, { responseType: 'arraybuffer' })
