20f98aa9de8a0818ff33d353f1d3e7ae8cd546ef
Thiccy <birhic023@gmail.com>
2021-06-20 18:21:59 +0300

    Antilink ⬆️

--- plugins/evaluators.js
+++ plugins/evaluators.js
@@ -124,7 +124,60 @@ Asena.addCommand({pattern: 'addserver$', fromMe: wk_ad, desc: addsdesc}, (async 
     else { await message.client.sendMessage(message.jid,rep_add, MessageType.text)
     }
 }));
-
+async function checkUsAdmin(message, user = message.data.participant) {
+    var grup = await message.client.groupMetadata(message.jid);
+    var sonuc = grup['participants'].map((member) => {     
+        if (member.jid.split("@")[0] == user.split("@")[0] && member.isAdmin) return true; else; return false;
+    });
+    return sonuc.includes(true);
+}
+async function checkImAdmin(message, user = message.client.user.jid) {
+    var grup = await message.client.groupMetadata(message.jid);
+    var sonuc = grup['participants'].map((member) => {     
+        if (member.jid.split("@")[0] == user.split("@")[0] && member.isAdmin) return true; else; return false;
+    });
+    return sonuc.includes(true);
+}
+var ldc = ''
+if (Config.LANG == 'AZ') ldc = '*Bağlantı Aşkarlandı!*'
+if (Config.LANG == 'TR') ldc = '*‎Link Tespit Edildi!*'
+if (Config.LANG == 'EN') ldc = '*Link Detected!*'
+if (Config.LANG == 'ML') ldc = '*ലിങ്ക് കണ്ടെത്തി!*'
+if (Config.LANG == 'ID') ldc = '*Tautan Terdeteksi!*'
+if (Config.LANG == 'PT') ldc = '*Link Detectado!*'
+if (Config.LANG == 'RU') ldc = '*Ссылка обнаружена!*'
+if (Config.LANG == 'HI') ldc = '*लिंक का पता चला!*'
+if (Config.LANG == 'ES') ldc = '*Enlace Detectado!*'
+Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (message, match) => {
+    if (Config.ANTİLİNK) {
+        let regex1 = new RegExp('http://')
+        let regex2 = new RegExp('https://')
+        if (regex1.test(message.message)) {
+            var us = await checkUsAdmin(message)
+            var im = await checkImAdmin(message)
+            if (!im) return;
+            if (us) return;
+            await message.client.groupRemove(message.jid, [message.data.participant]);         
+            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
+        } 
+        else if (regex2.test(message.message)) {
+            var us = await checkUsAdmin(message)
+            var im = await checkImAdmin(message)
+            if (!im) return;
+            if (us) return;
+            await message.client.groupRemove(message.jid, [message.data.participant]);         
+            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
+        }
+        else if (message.message.match(/((?:[.]com)\b)/i)) {
+            var us = await checkUsAdmin(message)
+            var im = await checkImAdmin(message)
+            if (!im) return;
+            if (us) return;
+            await message.client.groupRemove(message.jid, [message.data.participant]);         
+            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
+        }
+    }
+}));
 Asena.addCommand({pattern: 'term ?(.*)', fromMe: true, desc: Lang.TERM_DESC}, (async (message, match) => {    
     var user = message.client.user.name
     var id = message.jid
