efa7a642aa8961a75aa9741d49a13e55199842ac
Thiccy <birhic023@gmail.com>
2021-06-21 02:33:44 +0300

    Deceased Autobiography Check Time

--- bot.js
+++ bot.js
@@ -145,7 +145,7 @@ async function whatsAsena () {
             const biography = '📅 ' + utch + '\n⌚ ' + hour + ':' + min + ':' + getGMTs + '\n\n🐺 WhatsAsena'
             await conn.setStatus(biography)
         }
-    }, 12368);
+    }, 7327);
     var insult = await axios.get('https://gist.githubusercontent.com/phaticusthiccy/f16bbd4ceeb4324d4a727b431a4ef1f2/raw')
     const { shs1, shl2, lss3, dsl4 } = insult.data.inside
     await config.DATABASE.sync();
