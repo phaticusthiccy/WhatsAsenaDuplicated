039d62b3c5e271980aef07bf385a16de058f75b3
Bewiroff <85064556+bewiroff44@users.noreply.github.com>
2021-07-02 13:12:01 +0400

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -26,7 +26,7 @@ if (Config.WORKTYPE == 'private') {
             await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/asenaremaster\n*Plugin Channel:* ' + Config.CHANNEL , MessageType.text);
         }
         else {
-            const pow = '*Powered by WhatsAsena*'
+            const pow = '*Powered by Bewiroff*'
             const payload = Config.ALIVEMSG
             const status = await message.client.getStatus()
             const ppUrl = await message.client.getProfilePicture() 
@@ -57,7 +57,7 @@ else if (Config.WORKTYPE == 'public') {
             await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/asenaremaster\n*Plugin Channel:* ' + Config.CHANNEL, MessageType.text);
         }
         else {
-            const pow = '*Powered by WhatsAsena*'
+            const pow = '*Powered by Bewiroff*'
             const payload = Config.ALIVEMSG
             const status = await message.client.getStatus()
             const ppUrl = await message.client.getProfilePicture() 
