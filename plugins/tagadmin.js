756a2febef8b43478816ad2c414eb5c2d37b9f6a
byadems <81581944+byadems@users.noreply.github.com>
2021-07-04 23:28:56 +0000

    Update tagadmin.js

--- plugins/tagadmin.js
+++ plugins/tagadmin.js
@@ -5,19 +5,19 @@ Instagram: www.instagram.com/kyrie.baran
 
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
-const Config = require('../config');
+const Config = ('../config');
 
 const Language = require('../language');
 const Lang = Language.getString('tagall');
 
 if (Config.WORKTYPE == 'private') {
-    Asena.addCommand({pattern: 'tagadmin$', fromMe: true, desc: Lang.TAGADMİN}, (async (message, match) => {
+    Asena.addCommand({pattern: 'tagadmin', fromMe: true, desc: Lang.TAGADMİN}, (async (message, match) => {
         let grup = await message.client.groupMetadata(message.jid);
         var jids = [];
         mesaj = '';
         grup['participants'].map(async (uye) => {
             if (uye.isAdmin) {
-                mesaj += '▫️@' + uye.id.split('@')[0] + '\n';
+                mesaj += '@' + uye.id.split('@')[0] + ' ';
                 jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
             }
         });
@@ -25,13 +25,25 @@ if (Config.WORKTYPE == 'private') {
     }));
 }
 else if (Config.WORKTYPE == 'public') {
-    Asena.addCommand({pattern: 'tagadmin$', fromMe: false, desc: Lang.TAGADMİN}, (async (message, match) => {
+    Asena.addCommand({pattern: 'tagadmin', fromMe: false, desc: Lang.TAGADMİN}, (async (message, match) => {
         let grup = await message.client.groupMetadata(message.jid);
         var jids = [];
         mesaj = '';
         grup['participants'].map(async (uye) => {
             if (uye.isAdmin) {
-                mesaj += '▫️@' + uye.id.split('@')[0] + '\n';
+                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        });
+        await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }));
+    Asena.addCommand({pattern: 'tagadmin', fromMe: true, desc: Lang.TAGADMİN}, (async (message, match) => {
+        let grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(async (uye) => {
+            if (uye.isAdmin) {
+                mesaj += '@' + uye.id.split('@')[0] + ' ';
                 jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
             }
         });
