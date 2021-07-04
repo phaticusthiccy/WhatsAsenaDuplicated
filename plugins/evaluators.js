2d03baadd2e2005cf2375f15cf6385ae06383fd2
byadems <81581944+byadems@users.noreply.github.com>
2021-07-04 23:20:34 +0000

    Update evaluators.js

--- plugins/evaluators.js
+++ plugins/evaluators.js
@@ -164,8 +164,8 @@ if (Config.LANG == 'HI') ldc = '*लिंक का पता चला!*'
 if (Config.LANG == 'ES') ldc = '*Enlace Detectado!*'
 Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (message, match) => {
     if (antilink_var == 'true' && message.jid !== '905511384572-1616356915@g.us') {
-        let regex1 = new RegExp('http://')
-        let regex2 = new RegExp('https://')
+        let regex1 = new RegExp('http://chat.whatsapp.com/')
+        let regex2 = new RegExp('https://chat.whatsapp.com/')
         if (regex1.test(message.message)) {
             var us = await checkUsAdmin(message)
             var im = await checkImAdmin(message)
