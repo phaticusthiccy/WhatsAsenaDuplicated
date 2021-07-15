79d1c51c763fc3825e315de28e924ace452ca4ae
farooqbot <85386300+farooqbot@users.noreply.github.com>
2021-07-15 12:25:04 +0530

    Create delete.js

---
+++ plugins/delete.js
@@ -0,0 +1,9 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const DEL_DESC = "Deletes The Replied Message Send By The Bot [ ✅️ Official External Plugin ]"
+
+Asena.addCommand({pattern: 'del', fromMe: false, desc: DEL_DESC}, (async (message, match) => {
+
+  await message.reply_message.delete();
+
+}));
