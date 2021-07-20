290e5482b0190b805df106d1ac7bc02874032183
Thiccy <birhic023@gmail.com>
2021-07-21 02:20:38 +0300

    New Wa Web Version for InRepo QR Code ⬆️

--- qr.js
+++ qr.js
@@ -14,7 +14,7 @@ const fs = require('fs');
 async function whatsAsena () {
     const conn = new WAConnection();
     const Session = new StringSession();  
-    conn.version = [2, 2119, 6]
+    conn.version = [2, 2123, 8]
     conn.logger.level = 'warn';
     conn.regenerateQRIntervalMs = 50000;
     
