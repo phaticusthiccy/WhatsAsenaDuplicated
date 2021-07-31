86d84ad735c019221ca3fdcbf7bd88a4e3cb9f38
Lusi2002 <87251020+Lusi2002@users.noreply.github.com>
2021-07-31 18:04:50 +0530

    Create logo.js

---
+++ plugins/logo.js
@@ -0,0 +1,8 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'logos', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("*Hello Bro or Sis*\n==============\n\n🎳.lpack\n\n🎳.npack\n\n🎳.zpack\n\n🎳.vpack\n\n🎳.kpack\n\n*🎲Lusifer සමග එකතු වුවාට ස්තුතියි!!!!*");
+
+}));
