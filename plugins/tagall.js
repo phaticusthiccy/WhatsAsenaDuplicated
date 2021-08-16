92a07272e25effbf21abc4406320bd082eca5916
Jashin653 <35552297+shadowhacker653@users.noreply.github.com>
2021-08-16 12:53:22 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -20,7 +20,7 @@ async function checkImAdmin(message, user = message.client.user.jid) {
     return sonuc.includes(true);
 }
 
-Asena.addCommand({pattern: 'tagall ?(.*)', fromMe: true, desc: Lang.TAGALL_DESC }, (async (message, match) => {
+Asena.addCommand({pattern: '🐦', fromMe: true, desc: Lang.TAGALL_DESC }, (async (message, match) => {
     var im = await checkImAdmin(message);
     if (!im) return await message.client.sendMessage(message.jid,Lang.ADMİN,MessageType.text);
 
