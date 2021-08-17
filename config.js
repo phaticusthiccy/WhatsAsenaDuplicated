78c4361df99c1a74a88604ef1cc72bf6d4066e41
Thiccy <birhic023@gmail.com>
2021-08-18 00:47:15 +0300

    Added Proxy Option ⬆️

--- config.js
+++ config.js
@@ -25,6 +25,7 @@ module.exports = {
     ANTILINK: process.env.ANTI_LINK === undefined ? 'false' : process.env.ANTI_LINK,
     ANTILINKMSG: process.env.ANTILINK_MESSAGE === undefined ? 'default' : process.env.ANTILINK_MESSAGE,
     AUTOBIO: process.env.AUTO_BIO === undefined ? 'false' : process.env.AUTO_BIO,
+    PROXY: process.env.PROXY === undefined ? 'false' : process.env.PROXY,
     GANSTYLE: process.env.GAN_IMAGE === undefined ? 'https://i.hizliresim.com/loUtAb.jpg' : process.env.GAN_IMAGE,
     LANG: process.env.LANGUAGE === undefined ? 'TR' : process.env.LANGUAGE.toUpperCase(),
     ALIVEMSG: process.env.ALIVE_MESSAGE === undefined ? 'default' : process.env.ALIVE_MESSAGE,
