17c12d68cd81dca22a8e5236d735405f32b14c02
Thiccy <birhic023@gmail.com>
2021-08-27 01:57:53 +0300

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -558,51 +558,55 @@ if (config.WORKTYPE == 'private') {
         await message.sendMessage(Buffer.from(img5.data), MessageType.image)
     }));
 
-    Asena.addCommand({ pattern: 'github ?(.*)', fromMe: true, desc: Glang.GİTHUB_DESC }, async (message, match) => {
-
-        const userName = match[1]
- 
-        if (userName === '') return await message.client.sendMessage(message.jid, Glang.REPLY, MessageType.text)
-
-        await axios
-          .get(`https://videfikri.com/api/github/?username=${userName}`)
-          .then(async (response) => {
-
-            const {
-              hireable,
-              company,
-              profile_pic,
-              username,
-              fullname, 
-              blog, 
-              location,
-              email,
-              public_repository,
-              biografi,
-              following,
-              followers,
-              public_gists,
-              profile_url,
-              last_updated,
-              joined_on,
-            } = response.data.result
-
-            const githubscrap = await axios.get(profile_pic, 
-              {responseType: 'arraybuffer',
-            })
-
-            const msg = `*${Glang.USERNAME}* ${username} \n*${Glang.NAME}* ${fullname} \n*${Glang.FOLLOWERS}* ${followers} \n*${Glang.FOLLOWİNG}* ${following} \n*${Glang.BİO}* ${biografi} \n*${Glang.REPO}* ${public_repository} \n*${Glang.GİST}* ${public_gists} \n*${Glang.LOCATİON}* ${location} \n*${Glang.MAİL}* ${email} \n*${Glang.BLOG}* ${blog} \n*${Glang.COMPANY}* ${company} \n*${Glang.HİRE}* ${hireable === "true" ? Glang.HİRE_TRUE : Glang.HİRE_FALSE} \n*${Glang.JOİN}* ${joined_on} \n*${Glang.UPDATE}* ${last_updated} \n*${Glang.URL}* ${profile_url}`
-
-            await message.sendMessage(Buffer.from(githubscrap.data), MessageType.image, { 
-              caption: msg,
-            })
-          })
-          .catch(
-            async (err) => await message.client.sendMessage(message.jid, Glang.NOT, MessageType.text),
-          )
-      },
-    )
-
+    Asena.addCommand({ pattern: 'github ?(.*)', fromMe: true, desc: Glang.GİTHUB_DESC, usage: 'github phaticusthiccy // github phaticusthiccy/Emacs-Train' }, (async (message, match) => {
+      if (match[1].includes('/')) {
+        var data = WhatsAsenaStack.github_repos(match[1])
+        var Msg = WhatsAsenaStack.github_message(config.LANG)
+        if (data.username == undefined) return await message.client.sendMessage(message.jid, Msg.not_found_repo, MessageType.text)
+        var payload = Msg.repo.username + data.username + '\n' +
+          Msg.repo.repo_name + data.repo_name + '\n' +
+          Msg.repo.repo_id + data.repo_id + '\n' +
+          Msg.repo.repo_desc + data.repo_desc + '\n' +
+          Msg.repo.created_at + data.created_at + '\n' +
+          Msg.repo.updated_at + data.updated_at + '\n' +
+          Msg.repo.fork + data.fork == true ? '✅\n' : '❌\n' +
+          Msg.repo.size + data.size + 'KB' + '\n' +
+          Msg.repo.star + data.star + '\n' +
+          Msg.repo.forks + data.forks + '\n' +
+          Msg.repo.watcher + data.watcher + '\n' +
+          Msg.repo.subscribers + data.subscribers + '\n' +
+          Msg.repo.language + data.language + '\n' +
+          Msg.repo.issues + data.issues + '\n' +
+          Msg.repo.has_lisance + data.has_lisance == false ? '❌\n' : '✅\n' +
+          Msg.repo.lisance_key + data.lisance_key + '\n' +
+          Msg.repo.lisance_name + data.lisance_name + '\n' +
+          Msg.repo.branch + data.branch
+        await message.client.sendMessage(massage.jid, payload, MessageType.text)
+      } else {
+        var data = WhatsAsenaStack.github_user(match[1])
+        var Msg = WhatsAsenaStack.github_message(config.LANG)
+        if (data.status == false) return await message.client.sendMessage(message.jid, Msg.not_found_user, MassageType.text)
+        var payload = Msg.user.username + data.username + '\n' +
+          Msg.user.name + data.name == 'null' ? '' + '\n' : data.name + '\n' + 
+          Msg.user.biography + data.biography == 'null' ? '' + '\n' : data.biography + '\n' +
+          Msg.user.created_at + data.created_at + '\n' +
+          Msg.user.last_update + data.last_update + '\n' +
+          Msg.user.id + data.id + '\n' +
+          Msg.user.repos + data.repos + '\n' +
+          Msg.user.gists + data.gists + '\n' +
+          Msg.user.location + data.location == 'null' ? '' + '\n' : data.location + '\n' +
+          Msg.user.following + data.following + '\n' +
+          Msg.user.follower + data.follower + '\n' +
+          Msg.user.hireable + data.hireable == 'null' ? Msg.cant_rent + '\n' : Msg.can_rent + '\n'
+          Msg.user.blog + data.blog == false ? '' + '\n' : data.blog + '\n' +
+          Msg.user.twitter + data.twitter == 'null' ? '' + '\n' : data.twitter + '\n' +
+          Msg.user.company + data.company == 'null' ? '' + '\n' : data.company + '\n' +
+          Msg.user.mail + data.mail == 'null' ? '' + '\n' : data.mail
+        var bf = await axios.get(data.image, {responseType:'arraybuffer'})
+        await message.sendMessage(Buffer.from(bf.data), MessageType.image, { caption: payload })
+      }
+    }));
+        
     Asena.addCommand({pattern: 'lyric ?(.*)', fromMe: true, desc: Slang.LY_DESC }, (async (message, match) => { 
 
         if (match[1] === '') return await message.client.sendMessage(message.jid, Slang.NEED, MessageType.text);
@@ -1025,50 +1029,54 @@ else if (config.WORKTYPE == 'public') {
         await message.sendMessage(Buffer.from(img5.data), MessageType.image)
     }));
 
-    Asena.addCommand({ pattern: 'github ?(.*)', fromMe: false, desc: Glang.GİTHUB_DESC }, async (message, match) => {
-
-        const userName = match[1]
- 
-        if (userName === '') return await message.client.sendMessage(message.jid, Glang.REPLY, MessageType.text)
-
-        await axios
-          .get(`https://videfikri.com/api/github/?username=${userName}`)
-          .then(async (response) => {
-
-            const {
-              hireable,
-              company,
-              profile_pic,
-              username,
-              fullname, 
-              blog, 
-              location,
-              email,
-              public_repository,
-              biografi,
-              following,
-              followers,
-              public_gists,
-              profile_url,
-              last_updated,
-              joined_on,
-            } = response.data.result
-
-            const githubscrap = await axios.get(profile_pic, 
-              {responseType: 'arraybuffer',
-            })
-
-            const msg = `*${Glang.USERNAME}* ${username} \n*${Glang.NAME}* ${fullname} \n*${Glang.FOLLOWERS}* ${followers} \n*${Glang.FOLLOWİNG}* ${following} \n*${Glang.BİO}* ${biografi} \n*${Glang.REPO}* ${public_repository} \n*${Glang.GİST}* ${public_gists} \n*${Glang.LOCATİON}* ${location} \n*${Glang.MAİL}* ${email} \n*${Glang.BLOG}* ${blog} \n*${Glang.COMPANY}* ${company} \n*${Glang.HİRE}* ${hireable === "true" ? Glang.HİRE_TRUE : Glang.HİRE_FALSE} \n*${Glang.JOİN}* ${joined_on} \n*${Glang.UPDATE}* ${last_updated} \n*${Glang.URL}* ${profile_url}`
-
-            await message.sendMessage(Buffer.from(githubscrap.data), MessageType.image, { 
-              caption: msg,
-            })
-          })
-          .catch(
-            async (err) => await message.client.sendMessage(message.jid, Glang.NOT, MessageType.text),
-          )
-      },
-    )
+    Asena.addCommand({ pattern: 'github ?(.*)', fromMe: false, desc: Glang.GİTHUB_DESC, usage: 'github phaticusthiccy // github phaticusthiccy/Emacs-Train' }, (async (message, match) => {
+      if (match[1].includes('/')) {
+        var data = WhatsAsenaStack.github_repos(match[1])
+        var Msg = WhatsAsenaStack.github_message(config.LANG)
+        if (data.username == undefined) return await message.client.sendMessage(message.jid, Msg.not_found_repo, MessageType.text)
+        var payload = Msg.repo.username + data.username + '\n' +
+          Msg.repo.repo_name + data.repo_name + '\n' +
+          Msg.repo.repo_id + data.repo_id + '\n' +
+          Msg.repo.repo_desc + data.repo_desc + '\n' +
+          Msg.repo.created_at + data.created_at + '\n' +
+          Msg.repo.updated_at + data.updated_at + '\n' +
+          Msg.repo.fork + data.fork == true ? '✅\n' : '❌\n' +
+          Msg.repo.size + data.size + 'KB' + '\n' +
+          Msg.repo.star + data.star + '\n' +
+          Msg.repo.forks + data.forks + '\n' +
+          Msg.repo.watcher + data.watcher + '\n' +
+          Msg.repo.subscribers + data.subscribers + '\n' +
+          Msg.repo.language + data.language + '\n' +
+          Msg.repo.issues + data.issues + '\n' +
+          Msg.repo.has_lisance + data.has_lisance == false ? '❌\n' : '✅\n' +
+          Msg.repo.lisance_key + data.lisance_key + '\n' +
+          Msg.repo.lisance_name + data.lisance_name + '\n' +
+          Msg.repo.branch + data.branch
+        await message.client.sendMessage(massage.jid, payload, MessageType.text)
+      } else {
+        var data = WhatsAsenaStack.github_user(match[1])
+        var Msg = WhatsAsenaStack.github_message(config.LANG)
+        if (data.status == false) return await message.client.sendMessage(message.jid, Msg.not_found_user, MassageType.text)
+        var payload = Msg.user.username + data.username + '\n' +
+          Msg.user.name + data.name == 'null' ? '' + '\n' : data.name + '\n' + 
+          Msg.user.biography + data.biography == 'null' ? '' + '\n' : data.biography + '\n' +
+          Msg.user.created_at + data.created_at + '\n' +
+          Msg.user.last_update + data.last_update + '\n' +
+          Msg.user.id + data.id + '\n' +
+          Msg.user.repos + data.repos + '\n' +
+          Msg.user.gists + data.gists + '\n' +
+          Msg.user.location + data.location == 'null' ? '' + '\n' : data.location + '\n' +
+          Msg.user.following + data.following + '\n' +
+          Msg.user.follower + data.follower + '\n' +
+          Msg.user.hireable + data.hireable == 'null' ? Msg.cant_rent + '\n' : Msg.can_rent + '\n'
+          Msg.user.blog + data.blog == false ? '' + '\n' : data.blog + '\n' +
+          Msg.user.twitter + data.twitter == 'null' ? '' + '\n' : data.twitter + '\n' +
+          Msg.user.company + data.company == 'null' ? '' + '\n' : data.company + '\n' +
+          Msg.user.mail + data.mail == 'null' ? '' + '\n' : data.mail
+        var bf = await axios.get(data.image, {responseType:'arraybuffer'})
+        await message.sendMessage(Buffer.from(bf.data), MessageType.image, { caption: payload })
+      }
+    }));
 
     Asena.addCommand({pattern: 'lyric ?(.*)', fromMe: false, desc: Slang.LY_DESC }, (async (message, match) => {
 
