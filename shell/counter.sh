5d962e6e595a93177035a05258a334a5cbe5ffd6
Thiccy <birhic023@gmail.com>
2021-09-02 12:37:21 +0300

    Update counter.sh

--- shell/counter.sh
+++ shell/counter.sh
@@ -1,15 +1,15 @@
-num=10
+num={NUMBER}
 case $num in
 100)
-echo "0-100" ;;
+echo "{100ECHO}" ;;
 200)
-echo "100-200" ;;
+echo "{200ECHO}" ;;
 300)
-echo "200-300" ;;
+echo "{300ECHO}" ;;
 400)
-echo "300-400" ;;
+echo "{400ECHO}" ;;
 500)
-echo "400-500" ;;
+echo "{500ECHO}" ;;
 *)
 echo "false" ;;
 esac
