7c9ca045f06cfd677419df1ef6144ec4c46667f5
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-09-15 20:14:43 +0530

    Added new Simi Artificial Intelligence System 🥳✅️

---
+++ plugins/simi artificial intelligence.js
@@ -0,0 +1,25 @@
+/* Copyright (C) 2020 Yusuf Usta.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+WhatsAsena - Yusuf Usta
+*/
+
+// Simi Artificial Intelligence Contributed By @TOXIC-DEVIL
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const got = require('got');
+const Config = require('../config');
+let td = Config.WORKTYPE == 'public' ? false : true
+
+Asena.addCommand({pattern: 'simi ?(.*)', fromMe: td, desc: Lang.SIMI_DESC}, async (message, match) => {
+	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
+	try {
+		const response = await got(url);
+		const json = JSON.parse(response.body);
+	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n*🤖 ' + Lang.BOT_DIVIDER +'* ```' + json.messages[0].response + '```\n\n' , MessageType.text);
+	} catch {
+		return await message.client.sendMessage(message.jid, Lang.NOT_FOUND_RESPONSE, MessageType.text);
+	}
+});
