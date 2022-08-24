64fc095e5498da868bb08948760c9d8e4587bc11
LevimodzKA <111886399+LevimodzKA@users.noreply.github.com>
2022-08-24 11:03:26 +0300

    Update Base.js

--- whatsasena/Base.js
+++ whatsasena/Base.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2022 Levimodz.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+Whatsmodz - Levimodz
 */
 
 'use strict';
