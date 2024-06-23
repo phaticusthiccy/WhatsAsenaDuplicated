37a9de27b2642c45737774aa9b24ffec3a95dcc6
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-06-23 22:47:36 +0530

    refactor

--- plugins/_plugin.js
+++ plugins/_plugin.js
@@ -6,55 +6,54 @@ const { PluginDB, installPlugin } = require('./sql/plugin');
 const Language = require('../language');
 const Lang = Language.getString('_plugin');
 
-Asena.addCommand({ pattern: 'install ?(.*)', fromMe: true, desc: Lang.INSTALL_DESC, usage: '{}install https://gist.github.com/.../...' }, (async (message, match) => {
-
-try {
-  if (match[1] == '') return await message.client.sendMessage(message.jid, { text: Lang.NEED_URL + '.install https://gist.github.com/phaticusthiccy/4232b1c8c4734e1f06c3d991149c6fbd', edit: message.key });
+Asena.addCommand({ pattern: 'install ?(.*)', fromMe: true, desc: Lang.INSTALL_DESC, usage: '{}install <raw_url>' }, (async (message, match) => {
   try {
-    var url = new URL(match[1]);
-  } catch {
-    return await message.client.sendMessage(message.jid, { text: Lang.INVALID_URL, edit: message.key });
-  }
-  if (url.host === 'gist.github.com') {
-    url.host = 'gist.githubusercontent.com';
-    url = url.toString() + '/raw';
-  } else {
-    url = url.toString();
-  }
-  var response = await got(url);
-  if (response.statusCode == 200) {
-    var plugin_name = response.body.match(/addCommand\({.*pattern: ["'](.*)["'].*}/);
-    if (plugin_name.length >= 1) {
-      plugin_name = "__" + plugin_name[1];
-    } else {
-      plugin_name = "__" + Math.random().toString(36).substring(8);
-    }
-
-    plugin_name = plugin_name.split(" ")[0]
-    fs.writeFileSync("./plugins/" + plugin_name + '.js', response.body);
+    if (match[1] == '') return await message.client.sendMessage(message.jid, { text: Lang.NEED_URL + '.install https://gist.github.com/phaticusthiccy/4232b1c8c4734e1f06c3d991149c6fbd', edit: message.key });
     try {
-      require("./" + plugin_name);
-    } catch (e) {
-      fs.unlinkSync("./plugins/" + plugin_name + '.js');
-      return await message.client.sendMessage(message.jid, { text: Lang.INVALID_PLUGIN + ' ```' + e + '```', edit: message.key });
+      var url = new URL(match[1]);
+    } catch {
+      return await message.client.sendMessage(message.jid, { text: Lang.INVALID_URL, edit: message.key });
     }
-    var plugins = (await PluginDB.findAll()).map((plugin) => plugin.dataValues.name);
-    if (plugins.includes(plugin_name)) {
-      return await message.client.sendMessage(message.jid, { text: Lang.DUPLICATE, edit: message.key });
-    } 
-    await installPlugin(url, plugin_name);
-    return await message.client.sendMessage(message.jid, { text: Lang.INSTALLED, edit: message.key });
-  } else {
-    return await message.client.sendMessage(message.jid, { text: Lang.INVALID_URL, edit: message.key });
+    if (url.host === 'gist.github.com') {
+      url.host = 'gist.githubusercontent.com';
+      url = url.toString() + '/raw';
+    } else {
+      url = url.toString();
+    }
+    let response = await got(url);
+    if (response.statusCode == 200) {
+      let plugin_name = response.body.match(/addCommand\({.*pattern: ["'](.*)["'].*}/);
+      if (plugin_name.length >= 1) {
+        plugin_name = "__" + plugin_name[1];
+      } else {
+        plugin_name = "__" + Math.random().toString(36).substring(8);
+      }
+
+      plugin_name = plugin_name.split(" ")[0]
+      fs.writeFileSync("./plugins/" + plugin_name + '.js', response.body);
+      try {
+        require("./" + plugin_name);
+      } catch (e) {
+        fs.unlinkSync("./plugins/" + plugin_name + '.js');
+        return await message.client.sendMessage(message.jid, { text: Lang.INVALID_PLUGIN + ' ```' + e + '```', edit: message.key });
+      }
+      let plugins = (await PluginDB.findAll()).map((plugin) => plugin.dataValues.name);
+      if (plugins.includes(plugin_name)) {
+        return await message.client.sendMessage(message.jid, { text: Lang.DUPLICATE, edit: message.key });
+      } 
+      await installPlugin(url, plugin_name);
+      return await message.client.sendMessage(message.jid, { text: Lang.INSTALLED, edit: message.key });
+    } else {
+      return await message.client.sendMessage(message.jid, { text: Lang.INVALID_URL, edit: message.key });
+    }
+  } catch (e) {
+    return console.log(e)
   }
-} catch (e) {
-  return console.log(e)
-}
 }));
 
 Asena.addCommand({ pattern: 'plugin$', fromMe: true, desc: Lang.PLUGIN_DESC }, (async (message, match) => {
-  var mesaj = Lang.INSTALLED_FROM_REMOTE;
-  var plugins = await PluginDB.findAll();
+  let mesaj = Lang.INSTALLED_FROM_REMOTE;
+  let plugins = await PluginDB.findAll();
   if (plugins.length < 1) {
     return await message.client.sendMessage(message.jid, { text: Lang.NO_PLUGIN, edit: message.key });
   } else {
@@ -71,7 +70,7 @@ Asena.addCommand({ pattern: 'remove(?: |$)(.*)', fromMe: true, desc: Lang.REMOVE
   if (match[1] === '') return await message.client.sendMessage(message.jid, { text: Lang.NEED_PLUGIN, edit: message.key });
   match[1] = match[1].startsWith('__') ? match[1] : '__' + match[1];
   try {
-    var plugin = await PluginDB.findAll({
+    let plugin = await PluginDB.findAll({
       where: {
         name: match[1]
       }
